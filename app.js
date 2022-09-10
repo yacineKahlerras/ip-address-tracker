5feab4a5e82adf910b4fdb1edcfdfc25786717ae
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-09-10 17:08:35 +0100

    ok

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-/** IP ADRESS */
+/**
+ * =====================
+ * =====================
+ *        ip adress
+ * =====================
+ * =====================
+ * */
 const apiUrl =
   "https://geo.ipify.org/api/v2/country,city?apiKey=at_d7WLCbpcwqu5L6ntRswFt4lx0HjMS&ipAddress=";
 
 /** extracting data */
-const dataExtract = async () => {
-  const data = await fetchData();
+const init = async (ip) => {
+  const data = await fetchData(ip);
   console.log(data);
   createMap(data.location.lat, data.location.lng);
 };
 
 /** fetch data */
-const fetchData = async () => {
-  let response = await fetch(`${apiUrl}`, {
+const fetchData = async (ip) => {
+  if (!ip) ip = "";
+  let response = await fetch(`${apiUrl}${ip}`, {
     cache: "no-cache",
   }).catch(() => {
     console.log("error");
@@ -20,14 +27,31 @@ const fetchData = async () => {
   return response;
 };
 
-/** MAP */
+/** initializes everything */
+init("");
+
+/**
+ * =====================
+ * =====================
+ *          MAP
+ * =====================
+ * =====================
+ * */
 const createMap = (lat, lng) => {
   var map = L.map("map").setView([lat, lng], 13);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "© OpenStreetMap",
   }).addTo(map);
+
+  /** marker */
   var marker = L.marker([lat, lng]).addTo(map);
 };
 
-dataExtract();
+/**
+ * =====================
+ * =====================
+ *         input
+ * =====================
+ * =====================
+ * */
